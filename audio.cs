f0889c25cb3be607497af13525b5aa70a674088e
Ranielly Affonso <161093152+raniellyaff@users.noreply.github.com>
2026-08-27 00:48:40 +0000

    gitconectado

--- audio.cs
+++ audio.cs
@@ -1,9 +1,12 @@
-public AudioSource source;
-    
-    void Start()
-    {
-        source = GetComponent<AudioSource>();
-    }
-    void OnCollisionEnter2D (Collision2D coll) {
-        source.Play();
-    }
+Vector3 playerPos = transform.position;
+Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+Vector3 dir = mousePos - playerPos;
+dir.Normalize();
+
+Vector3 speedVec = dir * speed;
+
+var vel = rb2d.velocity;
+        vel.x = speedVec.x;
+        vel.y = speedVec.y;
+        rb2d.velocity = vel;
